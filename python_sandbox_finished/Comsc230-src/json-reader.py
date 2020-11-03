352bc358a0caa58ba042f683f855b5f7b28fad3e
Nathan Almeida <nalmeida345@g.rwu.edu>
2020-11-02 19:41:07 -0500

    commit on 'master'

--- python_sandbox_finished/Comsc230-src/json-reader.py
+++ python_sandbox_finished/Comsc230-src/json-reader.py
@@ -46,7 +46,7 @@ input_json = """
 
 fileName= 'employees.json'
 
-folderPath = '/Users/tnewman/Documents/GitHub/RWU/Comsc230/Examples/Python/DataFiles/'
+folderPath = '/GitHub/RWU/COMSC230/Python/Data Files'
 filePath = folderPath+'/'+fileName
 
 ####  Open and read the raw data from the JSON file
